6b31b71ecca16bacbf96a3d151933c8285d2782d
Esra404 <esradrmz571@gmail.com>
2023-05-06 17:10:37 +0300

    sinema bilet

--- SinemaKoltukRezervasyonu/script.js
+++ SinemaKoltukRezervasyonu/script.js
@@ -3,6 +3,8 @@ const count =document.getElementById('count');
 const amount =document.getElementById('amount');
 const select =document.getElementById('movie');
 const seats =document.querySelectorAll('.seat:not(.reserved)'); 
+getFromLocalStorage();
+calculateTotal();
 
 container.addEventListener('click',function(e){
     if(e.target.classList.contains('seat')&& !e.target.classList.contains('reserved')){
@@ -41,4 +43,28 @@ function calculateTotal(){
     let selectedSeatCount =selectedSeats.length;
     count.innerText =selectedSeatCount;
     amount.innerText=selectedSeatCount * select.value;
+    saveToLocalStorage(selectedSeatIndex);
+}
+
+
+function getFromLocalStorage(){
+    const selectedSeats =JSON.parese(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !=null && selectedSeats.length >0 ){
+        seats.forEach(function(seat,index) {
+            if (selectedSeats.indexOf(index) >-1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+}
+const selecttedMovieIndex=localStorage.getItem('selectedMovieIndex');
+if(selecttedMovieIndex != null){
+    select.selectedIndex =selecttedMovieIndex;
+}
+
+
+function saveToLocalStronge(indexs){
+    localStorage.setItem('selectedSeats',JSON.stringify(indexs));
+    localStorage.setItem('selecttedMovieIndex',select.selectedIndex);
 }
