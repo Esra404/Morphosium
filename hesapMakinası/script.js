c5d76a406fbfa2486460f444a173c407dbbb0b4b
Esra404 <esradrmz571@gmail.com>
2023-05-08 11:51:48 +0300

    operator girişleri

--- hesapMakinası/script.js
+++ hesapMakinası/script.js
@@ -2,6 +2,9 @@ const display =document.querySelector('.calculator-input');
 const keys =document.querySelector('.calculator-keys');
 
 let displayValue='0';
+let firstValue=null;
+let operator =null;
+let waitingForSecondValue=false;
 
 
 updateDisplay();
@@ -14,15 +17,20 @@ keys.addEventListener('click',function(e){
     if (!element.matches('button')) return;
 
     if(element.classList.contains('operator')){
-        console.log('operator',element.value);
+      //  console.log('operator',element.value);
+      handleOperator(element.value);
         return;
     }
     if(element.classList.contains('decimal')){
-        console.log('decimal',element.value);
+       // console.log('decimal',element.value);
+       inputDecimal();
+       updateDisplay();
         return;
     }
     if(element.classList.contains('clear')){
-        console.log('clear',element.value);
+       // console.log('clear',element.value);
+       clear ();
+       updateDisplay();
         return;
     }
 
@@ -31,6 +39,17 @@ keys.addEventListener('click',function(e){
    inputNumber(element.value);
    updateDisplay();
 });
+function handleOperator(operator){
+
+}
 function inputNumber(num){
-    displayValue =num;
+    displayValue =displayValue ==='0'? num: displayValue + num; 
+}
+function inputDecimal() {
+    if(!displayValue.includes('.')){
+        displayValue += '.';
+    }
+}
+function clear(){
+    displayValue='0';
 }
