5cde403e8006f60bcd30cf6576a245e3112ab85d
Esra404 <esradrmz571@gmail.com>
2023-05-09 20:20:31 +0300

    operator girişleri

--- hesapMakinası/script.js
+++ hesapMakinası/script.js
@@ -19,6 +19,7 @@ keys.addEventListener('click',function(e){
     if(element.classList.contains('operator')){
       //  console.log('operator',element.value);
       handleOperator(element.value);
+      updateDisplay();
         return;
     }
     if(element.classList.contains('decimal')){
@@ -39,12 +40,47 @@ keys.addEventListener('click',function(e){
    inputNumber(element.value);
    updateDisplay();
 });
-function handleOperator(operator){
+function handleOperator(nextOperator){
+    const value=parseFloat(displayValue);
+    if (operator && waitingForSecondValue){
+        operator=nextOperator;
+        return;
+    }
+
+    if (firstValue==null){
+        firstValue =value;
+    }
+    else if(operator){
+        const result =calculate(firstValue,value,operator);
+        displayValue =String(result);
+        firstValue=result;
+    }
+    waitingForSecondValue =true;
+    operator=nextOperator;
+    
+    console.log(displayValue,firstValue,operator,waitingForSecondValue);
 
 }
+function calculate(first,second,operator){
+    if (operator === '+'){
+        return first +second;
+    }else if (operator === '-'){
+        return first - second;
+    }else if (operator =='*'){
+        return first*second;
+    }else if (operator=='/'){
+        return first / second;
+    }
+    return second;
+}
 function inputNumber(num){
+    if(waitingForSecondValue){
+        displayValue=num;
+        waitingForSecondValue=false;
+    }
     displayValue =displayValue ==='0'? num: displayValue + num; 
 }
+console.log(displayValue,firstValue,operator,waitingForSecondValue);
 function inputDecimal() {
     if(!displayValue.includes('.')){
         displayValue += '.';
